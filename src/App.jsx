dfbdafcb5c681f4362e45d3a4fa9b066a38dba7f
shokhsanam18 <shokhsanam18@gmail.com>
2025-03-10 22:43:55 +0500

    feat:tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,9 @@
-
-
 function App() {
-  
-
   return (
     <>
-    <h1>hello client</h1>
+      <h1 className="bg-red-800">hello client</h1>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
