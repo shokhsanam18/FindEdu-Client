96eccd6e13a267e147ee1ca19b30383ea06ee0c3
Фарангиз Хуррамова <farangizkhurramova@Farangizs-MacBook-Air.local>
2025-03-11 23:10:52 +0500

    Register/Login pages are  created

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,20 @@
-import { Button } from "./components/ui/button";
+import React from "react";
+import { Button } from "./components/ui/button"; 
+import { Routes, Route } from "react-router-dom";
+import Register from "./Register/Login/Register.jsx";
+import Login from "./Register/Login/Login.jsx";
 
 function App() {
   return (
-    <>
+    <div>
       <h1 className="bg-red-800">hello client</h1>
       <Button>Hello world</Button>
-    </>
+      <Routes>
+        <Route path="/Register" element={<Register />} />
+        <Route path="/Login" element={<Login />} />
+        <Route path="*" element={<h1>Not Found</h1>} />
+      </Routes>
+    </div>
   );
 }
 
