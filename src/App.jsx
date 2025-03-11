bc3f1250d811fecbab3ff48d06148552b059dec1
shokhsanam18 <shokhsanam18@gmail.com>
2025-03-11 14:22:38 +0500

    feat: shadcn added

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,10 @@
+import { Button } from "./components/ui/button";
+
 function App() {
   return (
     <>
       <h1 className="bg-red-800">hello client</h1>
+      <Button>Hello world</Button>
     </>
   );
 }
