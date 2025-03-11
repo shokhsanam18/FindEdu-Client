d2045b425f59b5ad6ca11dd622a686c79bcbdff8
Фарангиз Хуррамова <farangizkhurramova@Farangizs-MacBook-Air.local>
2025-03-12 02:41:47 +0500

    register page logic

--- src/App.jsx
+++ src/App.jsx
@@ -7,12 +7,11 @@ import Login from "./Register/Login/Login.jsx";
 function App() {
   return (
     <div>
-      <h1 className="bg-red-800">hello client</h1>
-      <Button>Hello world</Button>
       <Routes>
-        <Route path="/Register" element={<Register />} />
+        <Route path="/" element={<Register />} />
         <Route path="/Login" element={<Login />} />
-        <Route path="*" element={<h1>Not Found</h1>} />
+        <Route path="/Forget" element={<h1>Forget password page.</h1>} />
+        <Route path="*" element={<h1>You logged in successfully.</h1>} />
       </Routes>
     </div>
   );
