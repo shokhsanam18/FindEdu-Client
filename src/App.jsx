f59d428e19a722bd7fc9ccc412a84cf0905c0114
shokhsanam18 <shokhsanam18@gmail.com>
2025-03-12 21:30:17 +0500

    fix:added index page

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from "react";
-import { Button } from "./components/ui/button"; 
-import { Routes, Route } from "react-router-dom";
+import { Button } from "./components/ui/button.jsx";
+import { Routes, Route, Link } from "react-router-dom";
 import Register from "./Register/Login/Register.jsx";
 import Login from "./Register/Login/Login.jsx";
 
@@ -8,7 +8,15 @@ function App() {
   return (
     <div>
       <Routes>
-        <Route path="/" element={<Register />} />
+        <Route
+          path="/"
+          element={
+            <Link to="/Register">
+              <Button>Register</Button>
+            </Link>
+          }
+        />
+        <Route path="/Register" element={<Register />} />
         <Route path="/Login" element={<Login />} />
         <Route path="/Forget" element={<h1>Forget password page.</h1>} />
         <Route path="*" element={<h1>You logged in successfully.</h1>} />
