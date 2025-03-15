c268493dfce1b02aa654d7e2e7d7c436614f9362
Fotimaa <fzikrillayeva2005@gmail.com>
2025-03-15 22:51:16 +0300

    Updated Navbar with responsive styles

--- src/App.jsx
+++ src/App.jsx
@@ -3,25 +3,31 @@ import { Button } from "./components/ui/button.jsx";
 import { Routes, Route, Link } from "react-router-dom";
 import Register from "./Register/Login/Register.jsx";
 import Login from "./Register/Login/Login.jsx";
+import Test from "./Test";
+
+
 
 function App() {
   return (
-    <div>
-      <Routes>
-        <Route
-          path="/"
-          element={
-            <Link to="/Register">
-              <Button>Register</Button>
-            </Link>
-          }
-        />
-        <Route path="/Register" element={<Register />} />
-        <Route path="/Login" element={<Login />} />
-        <Route path="/Forget" element={<h1>Forget password page.</h1>} />
-        <Route path="*" element={<h1>You logged in successfully.</h1>} />
-      </Routes>
-    </div>
+ 
+      <div>
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <Link to="/Register">
+                <Button>Register</Button>
+              </Link>
+            }
+          />
+          <Route path="/Register" element={<Register />} />
+          <Route path="/Login" element={<Login />} />
+          <Route path="/Forget" element={<h1>Forget password page.</h1>} />
+          <Route path="/test" element={<Test />} />
+          <Route path="*" element={<h1>You logged in successfully.</h1>} />
+        </Routes>
+      </div>
+    
   );
 }
 
---
+++ src/Test.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Navbar from './components/Navbar';
+
+function Test() {
+  return (
+    <div>
+      <Navbar />
+      <h1>This is the Test Page</h1>
+      <p>Your content will go here.</p>
+    </div>
+  );
+}
+
+export default Test;
