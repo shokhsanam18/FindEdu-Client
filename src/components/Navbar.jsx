5d2ad1f843efb210aec25cc3c12be1201a3153a4
InobatMi <inobatmirahmedova@gmail.com>
2025-03-28 22:56:21 +0500

    fix: ui fixed on navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -188,7 +188,7 @@ export default function Navbar() {
         {isLoggedIn ? 
           (
             <Menu open={isMenuOpen} handler={setIsMenuOpen} placement="bottom-end">
-            <MenuHandler>
+            <MenuHandler className='p-0 flex items-center gap-1.5'>
               <Button
                 variant="text"
                 color="blue-gray"
@@ -201,7 +201,7 @@ export default function Navbar() {
                   className="border border-purple-900 p-0.5"
                   src={profileImageUrl || 'https://images.unsplash.com/photo-1633332755192-727a05c4013d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1480&q=80'} // fallback if image fails
                 />
-                <motion.p className="text-[#290a3f]">{user?.data?.firstName} {user?.data?.lastName}</motion.p>
+                <p className="text-[#290a3f]">{user?.data?.firstName} {user?.data?.lastName}</p>
                 <ChevronDownIcon
                   strokeWidth={2.5}
                   className={`h-3 w-3 text-[#290a3f] transition-transform ${
@@ -210,7 +210,7 @@ export default function Navbar() {
                 />
               </Button>
             </MenuHandler>
-            {/* <MenuList className="p-1">
+            <MenuList className="p-1">
               {profileMenuItems.map(({ label, icon, link }, key) => {
                 const isLastItem = key === profileMenuItems.length - 1;
                 return (
@@ -241,7 +241,7 @@ export default function Navbar() {
                       </Link>
                 );
               })}
-            </MenuList> */}
+            </MenuList>
           </Menu>) 
           :
         
