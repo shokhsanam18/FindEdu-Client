f8673218d1eb4555ba47ec39ba0279b3606436a2
InobatMi <inobatmirahmedova@gmail.com>
2025-03-28 22:33:53 +0500

    fix: img added on navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -199,7 +199,7 @@ export default function Navbar() {
                   size="sm"
                   alt="User profile"
                   className="border border-purple-900 p-0.5"
-                  src={profileImageUrl || "/fallback-avatar.jpg"} // fallback if image fails
+                  src={profileImageUrl || 'https://images.unsplash.com/photo-1633332755192-727a05c4013d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1480&q=80'} // fallback if image fails
                 />
                 <motion.p className="text-[#290a3f]">{user?.data?.firstName} {user?.data?.lastName}</motion.p>
                 <ChevronDownIcon
